ed2e940b72ba125c219e3c0ef5e03606125f3ac6
Lissette Lindao <lnlindao@gmail.com>
2022-10-06 11:39:17 -0500

    ref(api) dj.js env variables added

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,42 @@ require("dotenv").config();
 const { Sequelize, Op } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, API_KEY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, API_KEY, DB_NAME } = process.env;
+
+/**
+ * CONEXION CON LA BD EN PRODUCCION
+ */
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+        logging: false, // set to console.log to see the raw SQL queries
+        native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      });
+
+/*
+
+UTILIZADO PARA CONECTAR CON LA BD EN LOCALHOST
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/railway`,
@@ -10,7 +45,8 @@ const sequelize = new Sequelize(
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
-);
+);*/
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
